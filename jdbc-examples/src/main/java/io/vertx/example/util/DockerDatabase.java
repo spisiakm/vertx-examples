9fd921e031f7c2a3b11c23867c19f454914a4a77
Martin Spisiak <mspisiak@redhat.com>
2018-07-26 10:21:37 +0200

    Fix condition

--- jdbc-examples/src/main/java/io/vertx/example/util/DockerDatabase.java
+++ jdbc-examples/src/main/java/io/vertx/example/util/DockerDatabase.java
@@ -31,7 +31,8 @@ public class DockerDatabase {
       "-e", "POSTGRES_USER=" + dbUser, "-e", "POSTGRES_PASSWORD=" + dbPassword, "-e", "POSTGRES_DB=" + dbName,
       "-p", "5432:5432", "-d", "postgres");
     Process process = processBuilder.start();
-    if (!process.waitFor(5, TimeUnit.MINUTES) || process.exitValue() != 0) {
+    boolean endedInTime = process.waitFor(5, TimeUnit.MINUTES);
+    if (!endedInTime || process.exitValue() != 0) {
       System.err.println((char) 27 + "[31mA posgres database init has failed!" + (char) 27 + "[0m");
       System.exit(1);
     }
